e0e2d5c071d7296a7f079fdcced179ce91b7ccf6
sahana s <sahanaone1@gmail.com>
2026-09-02 22:58:29 +0530

    Add 1013_fibonacci_number.java - LeetCode Problem 1013

---
+++ 1013_fibonacci_number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int fib(int n) {
+        
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        if(n== 1 || n == 0) return n;
+        if(dp[n] != -1) return dp[n];
+        dp[n] = fib(n-1) + fib(n-2);
+        return dp[n];
+    }
+}

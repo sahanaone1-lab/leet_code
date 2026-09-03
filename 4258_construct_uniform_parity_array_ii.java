de587be8509582f6a31c582d7fd636f403e62bfe
sahana s <sahanaone1@gmail.com>
2026-09-03 22:52:10 +0530

    Add 4258_construct_uniform_parity_array_ii.java - LeetCode Problem 4258

---
+++ 4258_construct_uniform_parity_array_ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min= Integer.MAX_VALUE;
+        for(int x: nums1){
+            min = Math.min(x,min);
+        }
+        if(min %2 ==1){
+            return true;
+        }
+        for(int x:nums1){
+            if(x%2==1)
+            return false;
+        }
+     return true;   
+    }
+}

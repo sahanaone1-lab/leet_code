aff1aa71b2a77d50dcf5f3e8ae8e5f607947239d
sahana s <sahanaone1@gmail.com>
2026-09-05 14:32:33 +0530

    Add 4285_smallest_stable_index_ii.java - LeetCode Problem 4285

---
+++ 4285_smallest_stable_index_ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n= nums.length;
+        int[] min = new int[n];
+        min[n-1] = nums[n-1];
+        for(int i=n-2; i>=0;i--){
+            min[i] = Math.min(nums[i],min[i+1]);
+        }
+        int max = nums[0];
+        for(int j = 0;j<n;j++){
+            max= Math.max(max,nums[j]);
+
+            if((max - min[j]) <= k){
+                return j;
+
+            }
+         }
+    return -1;
+}
+}

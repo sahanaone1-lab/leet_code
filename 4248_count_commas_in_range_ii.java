fa67d4be830fc7ed0b8553629ffae6a2983480c6
sahana s <sahanaone1@gmail.com>
2026-09-09 22:59:37 +0530

    Update 4248_count_commas_in_range_ii.java - LeetCode Problem 4248

--- 4248_count_commas_in_range_ii.java
+++ 4248_count_commas_in_range_ii.java
@@ -1,12 +1,38 @@
 class Solution {
     public long countCommas(long n) {
-        if((n>=1000 )&& (n<=9999)){
-            return n-999;
+
+        if (n < 1000) {
+            return 0;
         }
-        else if((n>=10000) && (n<=99999))
-        return n-10000  +1;
-        else if((n>=100000) && (n<=999999))
-        return n-100000 +1;
-        return 0;
+
+        else if (n < 1000000) {
+            return n - 999;
+        }
+
+        else if (n < 1000000000L) {
+            return 999000L
+                 + (n - 999999L) * 2;
+        }
+
+        else if (n < 1000000000000L) {
+            return 999000L
+                 + 999000000L * 2
+                 + (n - 999999999L) * 3;
+        }
+
+        else if (n < 1000000000000000L) {
+            return 999000L
+                 + 999000000L * 2
+                 + 999000000000L * 3
+                 + (n - 999999999999L) * 4;
+        }
+
+        else {
+            return 999000L
+                 + 999000000L * 2
+                 + 999000000000L * 3
+                 + 999000000000000L * 4
+                 + (n - 999999999999999L) * 5;
+        }
     }
 }
